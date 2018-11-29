834249d102e4ba435a4ede529cb37443d78ef85b
Shr Ying Pan <apppeterpan@gmail.com>
2018-11-29 14:25:30 +0800

    Initial Commit

---
+++ LoveChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveChat
+//
+//  Created by SHIH-YING PAN on 2018/11/29.
+//  Copyright © 2018 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
